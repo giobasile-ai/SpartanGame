0856787ec50a8d648cd3de48096e4e9cac93355c
giobasile-ai <giobasile@gmail.com>
2025-12-30 20:33:14 +0100

    feat: Update PWA assets and manifest
    Updates the PWA manifest and service worker to use new Spartan helmet icons and simplifies PWA metadata. Also cleans up some PWA-related meta tags in index.html.

--- sw.js
+++ sw.js
@@ -1,44 +1,33 @@
 
-const CACHE_NAME = 'spartan-v2';
+const CACHE_NAME = 'spartan-v3';
 const ASSETS = [
-  './',
   './index.html',
-  './index.tsx',
   './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700;900&family=Metamorphous&display=swap',
-  'https://cdn-icons-png.flaticon.com/192/9430/9430310.png',
-  'https://cdn-icons-png.flaticon.com/512/9430/9430310.png'
+  'https://img.icons8.com/fluency/192/spartan-helmet.png',
+  'https://img.icons8.com/fluency/512/spartan-helmet.png'
 ];
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then((keys) => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) return cachedResponse;
-      return fetch(event.request).catch(() => {
-        if (event.request.mode === 'navigate') {
-          return caches.match('./index.html');
-        }
-      });
+      return cachedResponse || fetch(event.request);
     })
   );
 });
